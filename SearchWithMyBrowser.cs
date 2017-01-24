060596fd10a9432404a77d1cf20983ad483c90f1
Charles Milette <charles.milette@gmail.com>
2017-01-24 11:16:00 -0500

    Removed restart requirement

--- SearchWithMyBrowser.cs
+++ SearchWithMyBrowser.cs
@@ -2,6 +2,26 @@ using Microsoft.Win32;
 using System.Diagnostics;
 
 public class SearchWithMyBrowser {
+
+    [Flags]
+    enum HChangeNotifyEventID
+    {
+        // ...
+        SHCNE_ASSOCCHANGED = 0x08000000
+        // ...
+    }
+
+    [Flags]
+    enum HChangeNotifyFlags
+    {
+        // ...
+        SHCNF_IDLIST = 0x0000
+        // ...
+    }
+
+    [DllImport("shell32.dll")]
+    static extern void SHChangeNotify(HChangeNotifyEventID wEventId, HChangeNotifyFlags uFlags, IntPtr dwItem1, IntPtr dwItem2);
+
     public static void Main(string[] CommandLine) {
         if (CommandLine[0] == "/register") {
             RegistryKey Software = Registry.LocalMachine.OpenSubKey("Software", true);
@@ -14,6 +34,7 @@ public class SearchWithMyBrowser {
             URLAssociations.SetValue("microsoft-edge", "SearchWithMyBrowser");
             RegistryKey RegisteredApplications = Software.OpenSubKey("RegisteredApplications", true);
             RegisteredApplications.SetValue("SearchWithMyBrowser", @"Software\SearchWithMyBrowser\Capabilities");
+            SHChangeNotify(HChangeNotifyEventID.SHCNE_ASSOCCHANGED, HChangeNotifyFlags.SHCNF_IDLIST, IntPtr.Zero, IntPtr.Zero);
         } else if (CommandLine[0] == "/unregister") {
             RegistryKey Software = Registry.LocalMachine.OpenSubKey("Software", true);
             RegistryKey RegisteredApplications = Software.OpenSubKey("RegisteredApplications", true);
@@ -21,6 +42,7 @@ public class SearchWithMyBrowser {
             Software.DeleteSubKeyTree("SearchWithMyBrowser");
             RegistryKey Classes = Software.OpenSubKey("Classes", true);
             Classes.DeleteSubKeyTree("SearchWithMyBrowser");
+            SHChangeNotify(HChangeNotifyEventID.SHCNE_ASSOCCHANGED, HChangeNotifyFlags.SHCNF_IDLIST, IntPtr.Zero, IntPtr.Zero);
         } else {
             Process.Start(CommandLine[0].Substring(15));
         }
