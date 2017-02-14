75b74611b6cd1c6e81a0ebee3bf677b156d6d311
Charles Milette <charles.milette@gmail.com>
2017-02-14 10:12:16 -0500

    Various improvements to makefiles

---
+++ Setup/Make.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+
+
+rem Make output directory
+mkdir "%~dp0..\Output\"
+echo.
+
+
+
+rem Clean older build
+del "%~dp0..\Output\SWMB-setup.exe"
+echo.
+
+
+
+rem Build the installer
+"%ProgramFiles(x86)%\Inno Setup 5\ISCC.exe" /Q "%~dp0InnoSetup.iss"
+echo.
