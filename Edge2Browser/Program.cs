8a6b1f095e9df4e6dc1186ebe275deb8197fd376
Charles Milette <charles.milette@gmail.com>
2017-02-14 09:25:46 -0500

    Fixed #18 because apparently I suck at security

--- Edge2Browser/Program.cs
+++ Edge2Browser/Program.cs
@@ -1,11 +1,32 @@
+using System;
 using System.Diagnostics;
+using System.Linq;
 
-namespace SearchWithMyBrowser {
-    public class Edge2Browser {
-        public static void Main(string[] CommandLine) {
-            if (CommandLine.Length > 0 && CommandLine[0].StartsWith("microsoft-edge:")) {
-                Process.Start(CommandLine[0].Substring(15));
-            }
-        }
-    }
+namespace SearchWithMyBrowser
+{
+	class Edge2Browser
+	{
+		static void Main(string[] CommandLine)
+		{
+			if (CommandLine.Length > 0 && CommandLine[0].StartsWith("microsoft-edge:", StringComparison.OrdinalIgnoreCase))
+			{
+				string LaunchURL = CommandLine[0].Substring(15);
+				if (LaunchURL.StartsWith("//"))
+				{
+					LaunchURL = LaunchURL.Substring(2);
+				}
+
+				string[] ValidProtocols = {"http://", "https://"};
+				if (!ValidProtocols.Any(ValidProtocol => LaunchURL.StartsWith(ValidProtocol, StringComparison.OrdinalIgnoreCase)))
+				{
+					LaunchURL = "http://" + LaunchURL;
+				}
+
+				if (new Uri(LaunchURL).IsWellFormedOriginalString())
+				{
+					Process.Start(new ProcessStartInfo(){FileName = LaunchURL});
+				}
+			}
+		}
+	}
 }
