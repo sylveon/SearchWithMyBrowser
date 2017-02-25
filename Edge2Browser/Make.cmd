70020828d730faa6e9f3c15e496f35424cf79265
Charles Milette <charles.milette@gmail.com>
2017-02-25 10:15:08 -0500

    Added some checks before deletes and directory creation.
    Removes clutter

--- Edge2Browser/Make.cmd
+++ Edge2Browser/Make.cmd
@@ -1,12 +1,16 @@
 @echo off
 
 rem Make output directory
-mkdir "%~dp0..\Output\"
-echo.
+IF NOT EXIST "%~dp0..\Output\" (
+  mkdir "%~dp0..\Output\"
+  echo.
+)
 
 rem Clean older build
-del "%~dp0..\Output\SearchWithMyBrowser.exe"
-echo.
+IF EXIST "%~dp0..\Output\SearchWithMyBrowser.exe" (
+  del "%~dp0..\Output\SearchWithMyBrowser.exe"
+  echo.
+)
 
 rem Build the executable
 "%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe" /nologo /target:winexe /optimize /nowin32manifest /win32icon:"%~dp0..\Resources\Edge2Browser.ico" /out:"%~dp0..\Output\SearchWithMyBrowser.exe" "%~dp0Program.cs"
--- Setup/Make.cmd
+++ Setup/Make.cmd
@@ -1,12 +1,16 @@
 @echo off
 
 rem Make output directory
-mkdir "%~dp0..\Output\"
-echo.
+IF NOT EXIST "%~dp0..\Output\" (
+  mkdir "%~dp0..\Output\"
+  echo.
+)
 
 rem Clean older build
-del "%~dp0..\Output\SWMB-setup.exe"
-echo.
+IF EXIST "%~dp0..\Output\SWMB-setup.exe" (
+  del "%~dp0..\Output\SWMB-setup.exe"
+  echo.
+)
 
 rem Build the installer
 "%ProgramFiles(x86)%\Inno Setup 5\ISCC.exe" /Q "%~dp0InnoSetup.iss"
