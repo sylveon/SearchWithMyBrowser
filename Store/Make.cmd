0e8bdaae4052009801f2c7db46c75d91c278f5e9
Charles Milette <charles.milette@gmail.com>
2017-04-08 16:35:01 -0400

    removed BOM, made makeappxsilent

--- Store/Make.cmd
+++ Store/Make.cmd
@@ -1,4 +1,4 @@
-﻿@echo off
+@echo off
 
 rem Make output directory
 IF NOT EXIST "%~dp0..\Output\" (
@@ -13,6 +13,6 @@ IF EXIST "%~dp0..\Output\StorePackage.appx" (
 )
 
 rem Build and sign the package
-"C:\Program Files (x86)\Windows Kits\10\bin\x64\makeappx.exe" pack /f "%~dp0Mapping.inf" /p "%~dp0..\Output\StorePackage.appx"
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\makeappx.exe" pack /f "%~dp0Mapping.inf" /p "%~dp0..\Output\StorePackage.appx" > nul
 "C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe" sign /q /fd SHA256 /f "%~dp0..\..\SWMB-key.pfx" "%~dp0..\Output\StorePackage.appx"
 echo.
