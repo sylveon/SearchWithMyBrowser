5945bc44d01bf7ef9979f3042db60a7a39a84dfe
Charles Milette <charles.milette@gmail.com>
2017-04-08 16:25:50 -0400

    safer script

--- Store/Make.cmd
+++ Store/Make.cmd
@@ -13,6 +13,6 @@ IF EXIST "%~dp0..\Output\StorePackage.appx" (
 )
 
 rem Build and sign the package
-"C:\Program Files (x86)\Windows Kits\10\bin\x64\makeappx.exe" pack /f Mapping.inf /p ..\Output\StorePackage.appx
-"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe" sign /a /v /fd SHA256 /f ..\..\SWMB-key.pfx ..\Output\StorePackage.appx
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\makeappx.exe" pack /f "%~dp0Mapping.inf" /p "%~dp0..\Output\StorePackage.appx"
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe" sign /a /v /fd SHA256 /f "%~dp0..\..\SWMB-key.pfx" "%~dp0..\Output\StorePackage.appx"
 echo.
