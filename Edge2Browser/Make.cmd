98d34200977ec043c0df95a8e6c29518adc80ea6
Charles Milette <charles.milette@gmail.com>
2017-04-08 15:45:40 -0400

    enabled high entropy ASLR and added a message box for when directly ran

--- Edge2Browser/Make.cmd
+++ Edge2Browser/Make.cmd
@@ -13,5 +13,5 @@ IF EXIST "%~dp0..\Output\SearchWithMyBrowser.exe" (
 )
 
 rem Build the executable
-"%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe" /nologo /target:winexe /optimize /nowin32manifest /win32icon:"%~dp0..\Resources\Edge2Browser.ico" /out:"%~dp0..\Output\SearchWithMyBrowser.exe" "%~dp0Program.cs"
+"%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe" /nologo /target:winexe /optimize /highentropyva /nowin32manifest /win32icon:"%~dp0..\Resources\Edge2Browser.ico" /out:"%~dp0..\Output\SearchWithMyBrowser.exe" "%~dp0Program.cs"
 echo.
--- Edge2Browser/Program.cs
+++ Edge2Browser/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Windows.Forms;
 
 namespace SearchWithMyBrowser
 {
@@ -8,16 +9,20 @@ namespace SearchWithMyBrowser
 	{
 		static void Main(string[] CommandLine)
 		{
-			if (CommandLine.Length > 0 && CommandLine[0].StartsWith("microsoft-edge:", StringComparison.OrdinalIgnoreCase))
+			if (CommandLine.Length == 0)
+			{
+				MessageBox.Show("Do a web search with Cortana to benefit of SearchWithMyBrowser!", "SearchWithMyBrowser", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+			}
+			else if (CommandLine.Length > 0 && CommandLine[0].StartsWith("microsoft-edge:", StringComparison.OrdinalIgnoreCase))
 			{
 				string LaunchURL = CommandLine[0].Substring(15);
+
 				if (LaunchURL.StartsWith("//"))
 				{
 					LaunchURL = LaunchURL.Substring(2);
 				}
 
-				string[] ValidProtocols = {"http://", "https://"};
-				if (!ValidProtocols.Any(ValidProtocol => LaunchURL.StartsWith(ValidProtocol, StringComparison.OrdinalIgnoreCase)))
+				if (!new string[] {"http://", "https://"}.Any(ValidProtocol => LaunchURL.StartsWith(ValidProtocol, StringComparison.OrdinalIgnoreCase)))
 				{
 					LaunchURL = "http://" + LaunchURL;
 				}
