f804d7e27369b6067949dc4c515018bfcf4b240d
Charles Milette <charles.milette@gmail.com>
2017-05-08 12:38:21 -0400

    Do not sign the package, as it is not required when submitting to Store

--- Store/Make.cmd
+++ Store/Make.cmd
@@ -12,7 +12,6 @@ IF EXIST "%~dp0..\Output\StorePackage.appx" (
   echo.
 )
 
-rem Build and sign the package
+rem Build the package
 "C:\Program Files (x86)\Windows Kits\10\bin\x64\makeappx.exe" pack /f "%~dp0Mapping.inf" /p "%~dp0..\Output\StorePackage.appx" > nul
-"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe" sign /q /fd SHA256 /f "%~dp0..\..\SWMB-key.pfx" "%~dp0..\Output\StorePackage.appx"
 echo.
