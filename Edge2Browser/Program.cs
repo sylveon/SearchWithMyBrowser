f3164d443660c6acad7e2ce44457f1a8b4c31d0b
Charles Milette <charles.milette@gmail.com>
2017-08-14 12:17:10 -0400

    Begin FCU work, reinstate PDB files

--- Edge2Browser/Program.cs
+++ Edge2Browser/Program.cs
@@ -10,27 +10,25 @@ namespace SearchWithMyBrowser
 		static void Main(string[] CommandLine)
 		{
 			if (CommandLine.Length == 0)
-			{
 				MessageBox.Show("Do a web search with Cortana to benefit of SearchWithMyBrowser!\n\nOr maybe come back later, there might be something new here ;)", "SearchWithMyBrowser", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
-			}
 			else if (CommandLine[0].StartsWith("microsoft-edge:", StringComparison.OrdinalIgnoreCase))
 			{
 				string LaunchURL = CommandLine[0].Substring(15);
 
-				if (LaunchURL.StartsWith("//"))
+				if (LaunchURL.StartsWith("?launchContext1=Microsoft.Windows.Cortana", StringComparison.OrdinalIgnoreCase)) // Handle FCU
 				{
-					LaunchURL = LaunchURL.Substring(2);
+					var ProtocolParameters = HttpUtility.ParseQueryString(LaunchURL);
+					LaunchURL = HttpUtility.UrlDecode(ProtocolParameters["url"]);
 				}
+
+				if (LaunchURL.StartsWith("//"))
+					LaunchURL = LaunchURL.Substring(2);
 
 				if (!new string[] {"http://", "https://"}.Any(ValidProtocol => LaunchURL.StartsWith(ValidProtocol, StringComparison.OrdinalIgnoreCase)))
-				{
 					LaunchURL = "http://" + LaunchURL;
-				}
 
 				if (new Uri(LaunchURL).IsWellFormedOriginalString())
-				{
 					Process.Start(new ProcessStartInfo(){FileName = LaunchURL});
-				}
 			}
 		}
 	}
