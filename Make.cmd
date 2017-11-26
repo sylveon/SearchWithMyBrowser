e51327c86cf962ee89f82b9d399f765aeb5424da
Charles Milette <charles.milette@gmail.com>
2017-11-26 15:41:34 -0500

    Add appveyor

--- Make.cmd
+++ Make.cmd
@@ -1,5 +1,7 @@
 @echo off
 
+if [%APPVEYOR%] NEQ [] path C:\Program Files (x86)\Windows Kits\10\bin\10.0.14393.0\x64;C:\Program Files (x86)\Inno Setup 5;%PATH%
+
 cd "%~dp0"
 
 rem Call all the make files from subdirectories
@@ -8,4 +10,4 @@ FOR /d %%i IN (*) DO (
         call %%i\Make.cmd
     )
 )
-pause
+if [%APPVEYOR%] EQ [] pause
